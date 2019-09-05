a3e99cc830ffb1cb2945eacf11dc66dd145f4db3
Echo <liuy218@wfu.edu>
2019-09-05 15:59:38 -0400

    Echo Liu's project

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
     int revVctr[8];               // User values
-    int i;                        // Loop index
+    int i;// Loop index
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
@@ -12,9 +12,16 @@ int main() {
     }
 
     // Reverse
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+
+    int temp;
+
+
+    for (i = 0; i < NUM_ELEMENTS/2; ++i) {
+        temp = revVctr[i];
+        revVctr[i] = revVctr[NUM_ELEMENTS - i - 1]; // Swap
+        revVctr[NUM_ELEMENTS - i - 1] = temp;
     }
+
 
     // Print values
     cout << endl << "New values: ";
